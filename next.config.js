913ce7285969d7dd5b104328fc017782b6b0e2d1
Lichanyeok <cksgur956@naver.com>
2023-08-21 20:18:09 +0900

    host setting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["https://github.com/Lichanyeok/DreamCatcher/blob/main/public"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,11 @@ export default function Home() {
     <div className="h-screen flex flex-col items-center">
       <div className="absolute w-screen z-10">
         <Image
-          src="https://github.com/Lichanyeok/DreamCatcher/blob/main/public/%EC%84%B8%EB%A1%9C.png"
+          src="/%E1%84%89%E1%85%A6%E1%84%85%E1%85%A9.png"
           alt="MainImg"
-          layout="responsive"></Image>
+          layout="responsive"
+          width={600}
+          height={600}></Image>
       </div>
       <div className="absolute z-20 h-1/6 w-10/12 bottom-1/3 p-4 border-t-4 border-solid border-textBoxTop rounded-b-2xl shadow-md text-white text-center  bg-textBox">
         <p>여긴 어떻게 찾아온거야?</p>
